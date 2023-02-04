7d6477c95460af20e8afbb91336e371c5e9f8344
Kairat Sultan <garri.sultonov@mail.ru>
2023-02-04 17:09:40 +0600

    admin page customizations

--- visa/admin.py
+++ visa/admin.py
@@ -2,16 +2,44 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(VisaCategory)
-admin.site.register(BuisnessCategory)
+@admin.register(VisaCategory)
+class VisaCategoryAdmin(admin.ModelAdmin):
+    list_display = ["country"]
+
+
+@admin.register(BuisnessCategory)
+class BuisnessCategoryAdmin(admin.ModelAdmin):
+    list_display = ["country"]
+
+
 admin.site.register(CitizenCategory)
 admin.site.register(Travel)
 admin.site.register(Images)
 admin.site.register(News)
 admin.site.register(Unnecessary)
 admin.site.register(HeaderSlider)
-admin.site.register(DityoSlider)
-admin.site.register(CountrySlider) 
-admin.site.register(Countries)
-admin.site.register(Continent)
-admin.site.register(Flag)
+
+
+@admin.register(DityoSlider)
+class DityoSliderAdmin(admin.ModelAdmin):
+    list_display = ["name_uz", "price"]
+
+
+@admin.register(CountrySlider)
+class CountrySliderAdmin(admin.ModelAdmin):
+    list_display = ["name_uz"]
+
+
+@admin.register(Countries)
+class CountriesAdmin(admin.ModelAdmin):
+    list_display = ["name_uz"]
+
+
+@admin.register(Continent)
+class ContinentAdmin(admin.ModelAdmin):
+    list_display = ["name_uz"]
+
+
+@admin.register(Flag)
+class FlagAdmin(admin.ModelAdmin):
+    list_display = ["name_uz"]
